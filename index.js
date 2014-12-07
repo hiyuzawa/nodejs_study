78693ce58a16b8b8edc2fbda6bbd38248db8964b
Hideto Yuzawa <hiyuzawa@gmail.com>
2014-12-07 23:26:53 +0900

    routerの作成

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 var server = require("./server")
+var router = require("./router")
+var requestHanders = require("./requestHanders") 
 
-server.start()
+var handle = {}
+handle["/"] = requestHanders.start
+handle["/start"] = requestHanders.start
+handle["/upload"] = requestHanders.upload
+
+server.start(router.route, handle)
